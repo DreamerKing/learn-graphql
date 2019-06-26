2c0e52cd5c628380a31cf04300d480df46fa7211
DreamerKing <dreamingking@live.cn>
2019-06-26 14:32:48 +0800

    6.26

---
+++ hello.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { graphql, buildSchema } = require("graphql");
+const bodyParser = require('body-parser');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+/* const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const root = { hello: () => "Hello world!" };
+
+const app = express();
+app.use('graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+})); */
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+
+    schema {
+        query: Query
+    }
+    `;
+
+const resolvers = {
+    Query: {
+        hello(root){
+            return "world";
+        }
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+server.applyMiddleware({ app });
+// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql'}));
+
+app.listen({ port: 4000 }, ()=> console.log("Now server to localhost:8080/graphql"));
+
+/* graphql(schema, '{hello}', root).then((response) => {
+    console.log(response);
+}); */
